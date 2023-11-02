9126ce3deb07699e530cebdf98b811c23f4549dd
las6 <lauri.suopera@gmail.com>
2023-11-02 18:32:58 +0200

    butcherin' api for next.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,18 @@
 // 'use client';
 import { Flow } from './Flow';
 import { makeInitialData, getLayoutedElements } from '../components/helpers'
+import { sitemap } from '../pages/api/sitemapper';
 
 // app/page.js
 // This file maps to the index route (/)
 
 async function getData() {
-	const res = await fetch('http://localhost:3000/api/sitemapper');
+	// const res = await fetch(`${process.env.URL}/api/sitemapper`);
+	// const res = await fetch(`${process.env.URL}/api/sitemapper`);
+	const res2 = sitemap();
 	// The return value is *not* serialized
 	// You can return Date, Map, Set, etc.
-	return res.json();
+	return res2;
 }
 
 
--- pages/api/sitemapper.js
+++ pages/api/sitemapper.js
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default async function handler(req, res) {
+export const sitemap = async () => {
 	const Sitemapper = require('sitemapper');
 	const sitemap = new Sitemapper();
 
@@ -12,8 +11,13 @@ export default async function handler(req, res) {
 	// const res2 = await sitemap.fetch('https://staging.hideoutvillas.com/sitemap.xml');
 	// const res2 = await sitemap.fetch('https://www.vaasan.fi/sitemap.xml');
 	const res2 = await sitemap.fetch('https://www.cricketlighters.com/sitemap.xml');
+	return res2;
+}
+
+
+export default async function handler(req, res) {
 	// const res2 = await sitemap.fetch('https://www.dqcomms.com/sitemap.xml');
 	// const res2 = await sitemap.fetch('localhost:1357');
-	res.status(200).json(res2);
+	res.status(200).json( await sitemap());
 }
 
